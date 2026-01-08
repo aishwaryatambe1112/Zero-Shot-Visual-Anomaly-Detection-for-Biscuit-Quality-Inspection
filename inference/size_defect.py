63798ab660d7afe85a743511b5ec02021c1b78ae
Aishwarya Tambe <tambeaishwarya11@gmail.com>
2026-01-08 17:05:58 +0530

    Add size defect checking function

--- inference/size_defect.py
+++ inference/size_defect.py
@@ -1,1 +1,14 @@
+EXPECTED_AREA = 8500
+TOLERANCE = 0.20
+
+def check_size_defect(area):
+    lower = EXPECTED_AREA * (1 - TOLERANCE)
+    upper = EXPECTED_AREA * (1 + TOLERANCE)
+
+    if area < lower:
+        return True, "undersized"
+    elif area > upper:
+        return True, "oversized"
+    else:
+        return False, "normal"
 
