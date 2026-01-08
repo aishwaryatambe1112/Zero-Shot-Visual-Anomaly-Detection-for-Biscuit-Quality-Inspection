41fed242f4e7f8a70be74d038fe8a78ab444b38d
Aishwarya Tambe <tambeaishwarya11@gmail.com>
2026-01-08 17:04:58 +0530

    Update preprocess.py

--- vision/preprocess.py
+++ vision/preprocess.py
@@ -1,1 +1,8 @@
+import cv2
+
+def preprocess_frame(frame):
+    frame = cv2.resize(frame, (640, 480))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    return blur
 
