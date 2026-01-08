2e177151e3728efab75e48b5539362150c5f7804
Aishwarya Tambe <tambeaishwarya11@gmail.com>
2026-01-08 17:07:37 +0530

    Update metrics.py

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -1,1 +1,25 @@
+from collections import defaultdict
+
+class Metrics:
+    def __init__(self, classes):
+        self.classes = classes
+        self.matrix = defaultdict(lambda: defaultdict(int))
+
+    def update(self, gt, pred):
+        self.matrix[gt][pred] += 1
+
+    def precision(self, cls):
+        tp = self.matrix[cls][cls]
+        fp = sum(self.matrix[c][cls] for c in self.classes if c != cls)
+        return tp / (tp + fp + 1e-6)
+
+    def recall(self, cls):
+        tp = self.matrix[cls][cls]
+        fn = sum(self.matrix[cls][c] for c in self.classes if c != cls)
+        return tp / (tp + fn + 1e-6)
+
+    def f1(self, cls):
+        p = self.precision(cls)
+        r = self.recall(cls)
+        return 2 * p * r / (p + r + 1e-6)
 
